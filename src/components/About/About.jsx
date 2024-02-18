b413f80cf2aedd9e280308283dd589d324d1adf6
Orkhan Aliyev <orkhan1aliyev@gmail.com>
2024-02-18 23:26:38 +0100

    Updated 18/02/2024

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,9 @@ import './about.css';
 
 // Assets
 import AboutEducation from '../../assets/images/about-education.png';
+import EducationIcon from '../../assets/icons/AboutEducation.svg';
+import ExperienceIcon from '../../assets/icons/AboutExperience.svg';
+import ExtraIcon from '../../assets/icons/AboutExtra.svg';
 
 const About = () => {
   return (
@@ -15,9 +18,13 @@ const About = () => {
         </div>
 
         <div className="about-blocks">
-            <div className="about-block d-flex align-items-center">
-                <div className="about-block-img">
+            <div className="about-block d-flex align-items-center mt-5">
+                {/* <div className="about-block-img">
                     <img src={AboutEducation} alt='about-education' />
+                </div> */}
+
+                <div className="about-block-icon">
+                  <img src={EducationIcon} />
                 </div>
 
                 <div className="about-block-content">
@@ -25,6 +32,42 @@ const About = () => {
 
                     <p className='mb-0'>
                         Çexiya, Praqada yerləşən Avropada ən nüfuzlu universitetlərindın biri olan Karlova Universitetində Kompyuter Elmləri üzrə Bakalavr təhsili almaqla yanaşı, qrupun ən aktiv və top şagirdlərindənəm!
+                    </p>
+                </div>
+            </div>
+
+            <div className="about-block d-flex align-items-center">
+                {/* <div className="about-block-img">
+                    <img src={AboutEducation} alt='about-education' />
+                </div> */}
+
+                <div className="about-block-icon">
+                  <img src={ExperienceIcon} />
+                </div>
+
+                <div className="about-block-content">
+                    <h5>Təcrübə</h5>
+
+                    <p className='mb-0'>
+                    Hələ də tələbə olduğuma baxmayaraq, bir neçə startup və şirkətlərdə tech sahəsində işləmiş, və sərbəst olaraq full-stack proyektlər üzərində çalışmışam!
+                    </p>
+                </div>
+            </div>
+
+            <div className="about-block d-flex align-items-center">
+                {/* <div className="about-block-img">
+                    <img src={AboutEducation} alt='about-education' />
+                </div> */}
+
+                <div className="about-block-icon">
+                  <img src={ExtraIcon} />
+                </div>
+
+                <div className="about-block-content">
+                    <h5>Əlavə :)</h5>
+
+                    <p className='mb-0'>
+                    Təcrübə və bacarıqlarım yalnız tech sahəsi ilə məhdudiyyətlənmir! Əlavə olaraq təhsil sahəsində də professional təcrübəm və yuxarı səviyyəli ünsiyyət bacarıqlarım ilə dərslərimiz daha asan və maraqlı keçəcək! 
                     </p>
                 </div>
             </div>
